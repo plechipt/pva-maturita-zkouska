eef46d259fff10268a936f2c38338ba03b9ebcbc
Jakub Plecháč <plechac@spstrutnov.cz>
2024-04-06 11:38:40 +0200

    asp init

---
+++ PvaMaturitaZkouska/PoznamkyAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PoznamkyAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
