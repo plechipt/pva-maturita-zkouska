6b476a195a91a5fd212b2e6c96395ad6f4bf1b0f
Jakub Plecháč <plechac@spstrutnov.cz>
2024-04-06 11:39:55 +0200

    delete

--- PvaMaturitaZkouska/PoznamkyAsp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PoznamkyAsp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
